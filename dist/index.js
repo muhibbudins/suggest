044e4ae7dc5802ce6f18c21b3428e7af04c14f90
Muhibbudin Suretno <muhibbudinsuretno1@gmail.com>
2018-10-13 11:40:29 +0700

    Initial commit

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -9,39 +9,62 @@ var Suggest = function () {
     _classCallCheck(this, Suggest);
 
     this.wrapper = app;
+    this.id = 'asdasd';
+    this.options = Object.assign({}, opts);
     this.start();
   }
 
   _createClass(Suggest, [{
-    key: "fetch",
-    value: function fetch() {
-      return new Promise(function (resolve) {});
+    key: 'template',
+    value: function template() {
+      return '\n      <div class="suggest-wrapper" data-suggest-id="' + this.id + '">\n        <input type="text" />\n        <div class="suggest-list"></div>\n      </div>\n    ';
     }
   }, {
-    key: "filter",
-    value: function filter() {
-      return new Promise(function (resolve) {});
-    }
-  }, {
-    key: "draw",
-    value: async function draw() {
-      // const fetch = await this.fetch()
-      // const result = await this.filter(fetch)
+    key: 'result',
+    value: function result(data, query) {
+      return data.map(function (item) {
+        var search = '(' + query.split(' ').join('|') + ')';
+        var regex = new RegExp(search, 'g');
+        var title = item['title'].toLowerCase().replace(regex, '<b>$1</b>');
 
-      return "\n      <div class=\"suggest-wrapper\">\n        <input type=\"text\" />\n        <div class=\"suggest-list\">\n          " + this.drawItem([]) + "\n        </div>\n      </div>\n    ";
+        return '<div class="suggest-list_item">' + title + '</div>';
+      }).join('\n');
     }
   }, {
-    key: "drawItem",
-    value: function drawItem() {}
-  }, {
-    key: "start",
-    value: function start() {
+    key: 'draw',
+    value: function draw(wrapper, query) {
       var _this = this;
 
-      this.draw().then(function (res) {
-        console.log(res);
-        document.querySelector(_this.wrapper).innerHTML = res;
+      fetch(this.options['url']).then(function (res) {
+        return res.json();
+      }).then(function (res) {
+        wrapper.innerHTML = _this.result(res, query);
       });
+    }
+  }, {
+    key: 'start',
+    value: function start() {
+      var _this2 = this;
+
+      var wrapper = document.querySelector(this.wrapper);
+
+      wrapper.innerHTML = this.template();
+
+      var element = document.querySelector('[data-suggest-id="' + this.id + '"]');
+      var input = element.querySelector('input');
+      var list = element.querySelector('.suggest-list');
+
+      input.addEventListener('keyup', function (e) {
+        var value = e.target.value;
+
+        if (value.length >= _this2.options['minimal']) {
+          list.classList.add('suggest-list_active');
+
+          _this2.draw(list, value);
+        } else {
+          list.classList.remove('suggest-list_active');
+        }
+      }, false);
     }
   }]);
 
--- index.js
+++ index.js
@@ -1,43 +1,57 @@
 class Suggest {
   constructor(app, opts) {
     this.wrapper = app
+    this.id = 'asdasd'
+    this.options = Object.assign({}, opts)
     this.start()
   }
 
-  fetch() {
-    return new Promise(resolve => {
-
-    })
-  }
-
-  filter() {
-    return new Promise(resolve => {
-
-    })
-  }
-
-  async draw() {
-    // const fetch = await this.fetch()
-    // const result = await this.filter(fetch)
-
+  template() {
     return `
-      <div class="suggest-wrapper">
+      <div class="suggest-wrapper" data-suggest-id="${this.id}">
         <input type="text" />
-        <div class="suggest-list">
-          ${this.drawItem([])}
-        </div>
+        <div class="suggest-list"></div>
       </div>
     `
   }
 
-  drawItem() {
+  result(data, query) {
+    return data.map(item => {
+      const search = `(${query.split(' ').join('|')})`
+      const regex = new RegExp(search, 'g')
+      const title = item['title'].toLowerCase().replace(regex, '<b>$1</b>')
 
+      return `<div class="suggest-list_item">${title}</div>`
+    }).join('\n')
+  }
+
+  draw(wrapper, query) {
+    fetch(this.options['url'])
+      .then(res => res.json())
+      .then(res => {
+        wrapper.innerHTML = this.result(res, query)
+      })
   }
 
   start() {
-    this.draw().then(res => {
-      console.log(res)
-      document.querySelector(this.wrapper).innerHTML = res
-    })
+    const wrapper = document.querySelector(this.wrapper)
+
+    wrapper.innerHTML = this.template()
+
+    const element = document.querySelector(`[data-suggest-id="${this.id}"]`)
+    const input = element.querySelector('input')
+    const list = element.querySelector('.suggest-list')
+
+    input.addEventListener('keyup', (e) => {
+      const value = e.target.value
+
+      if (value.length >= this.options['minimal']) {
+        list.classList.add('suggest-list_active')
+
+        this.draw(list, value)
+      } else {
+        list.classList.remove('suggest-list_active')
+      }
+    }, false)
   }
 }
